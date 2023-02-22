57887eb2551398181fc4b1d096e766a3b0b26c57
samstr <samstr@gmail.com>
2023-02-22 12:18:37 +0000

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <Link href="/signup">Signup test</Link>
+      <Link href="/login">Login test</Link>
+      Index test placeholder
+    </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <Link href="/signup">Signup test</Link>
+      <Link href="/login">Login test</Link>
+      Signup test placeholder
+    </div>
+  );
+}
